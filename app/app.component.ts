0a7e80bf422b2dda08b21e700229f53d24c2022e
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-19 12:56:05 -0700

    create root component and root module

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>List of Zoo Animals. Last update: {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentAnimal}}</h3>
+  </div>
+  `
+})
+
+export class AppComponent {
+  currentAnimal: string = 'Arctic Fox';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+}
