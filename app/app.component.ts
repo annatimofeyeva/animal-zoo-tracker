1e326954118665cc918d927cbedded5c8e852ded
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-20 10:39:55 -0700

    add form for Animal update

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,8 +8,21 @@ import { Component } from '@angular/core';
     <h1>List of Zoo's Animals. Last update: {{month}}/{{day}}/{{year}}</h1>
 <br>
 <ul>
-  <li [class]="priorityColor(currentAnimal)" (click)="isDone(currentAnimal)" *ngFor="let currentAnimal of animals">{{currentAnimal.species}}, {{currentAnimal.name}} <button (click)="editAnimal()">Edit!</button></li>
+  <li [class]="priorityColor(currentAnimal)" (click)="isDone(currentAnimal)" *ngFor="let currentAnimal of animals">{{currentAnimal.species}}, {{currentAnimal.name}} <button (click)="editAnimal(currentAnimal)">Edit!</button></li>
 </ul>
+<hr>
+        <div>
+          <h3>{{selectedAnimal.species}}</h3>
+          <p>Animal Complete? {{selectedAnimal.done}}</p>
+          <h3>Edit Animal</h3>
+          <label>Enter Animal Species:</label>
+          <input [(ngModel)]="selectedAnimal.species">
+          <label>Change Animal Location:</label>
+          <br>
+          <input type="radio" [(ngModel)]="selectedAnimal.diet" [value]="Carnivore">Carnivore<br>
+          <input type="radio" [(ngModel)]="selectedAnimal.diet" [value]="Herbivore">Herbivore<br>
+          <button (click)="finishedEditing()">Done</button>
+        </div>
   </div>
   `
 })
@@ -25,20 +38,38 @@ export class AppComponent {
     new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
     new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud Noises")
   ];
+   selectedAnimal: Animal = this.animals[0];
 
-  editAnimal() {
-      alert("You just requested to edit a Task!");
+   editAnimal(clickedAnimal) {
+      this.selectedAnimal = clickedAnimal;
+    }
+
+    isDone(clickedAnimal: Animal) {
+        if(clickedAnimal.done === true) {
+          alert("This animal is done!");
+        } else {
+          alert("This animal is not done.");
+        }
+      }
+
+    finishedEditing() {
+      this.selectedAnimal = null;
+  }
+
+   editTask(clickedAnimal) {
+      this.selectedAnimal = clickedAnimal;
     }
 
     priorityColor(currentAnimal){
-    if (currentAnimal.location === 'Northern Trail'){
+    if (currentAnimal.diet === 'Carnivore'){
       return "bg-warning";
-    } else {
+    } else if (currentAnimal.diet === 'Herbivore'){
       return "bg-info";
     }
   }
 }
 
 export class Animal {
+  public done: boolean = false;
   constructor(public species: string, public  name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string){}
 }
