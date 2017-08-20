5a536879d70bc59a367dc31aa09039d6700c3d18
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-20 16:35:48 -0700

    edit animal fixed

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,85 +4,68 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-  <br>
-    <h1>List of Zoo's Animals:</h1><br>
-    <h2>Last update: {{month}}/{{day}}/{{year}}</h2>
-<br>
-<br>
-<ul>
-  <li [class]="priorityColor(currentAnimal)" (click)="isDone(currentAnimal)" *ngFor="let currentAnimal of animals">{{currentAnimal.species}},   {{currentAnimal.name}},   {{currentAnimal.age}},   {{currentAnimal.location}},   {{currentAnimal.caretakers}} <button  (click)="editAnimal(currentAnimal)">Edit!</button></li>
-</ul>
-<hr>
-<div *ngIf="selectedAnimal">
-        <div>
-          <h3>{{selectedAnimal.species}}</h3>
-          <p>Animal Complete? {{selectedAnimal.done}}</p>
-          <h3>Edit Animal</h3>
-          <label>Enter Animal Species:</label>
-          <input [(ngModel)]="selectedAnimal.species">
-          <br>
-          <label>Enter Animal Name:</label>
-          <input [(ngModel)]="selectedAnimal.name">
-          <br>
-          <label>Enter Animal Age:</label>
-          <input type="number" name="age" [(ngModel)]="selectedAnimal.age">
-          <br>
-          <label>Enter Animal Location:</label>
-          <input [(ngModel)]="selectedAnimal.location">
-          <br>
-          <label>Enter Animal Caretakers:</label>
-          <input [(ngModel)]="selectedAnimal.caretakers">
-          <br>
-        </div>
-          <button (click)="finishedEditing()">Done</button>
-    </div>
+      <h1>Zoo Animal's List: </h1>
+      <h3>Last Update:  {{month}}/{{day}}/{{year}}</h3>
+      <h3>{{currentList}}</h3>
+      <ul class="list-group">
+       <li class="list-group-item" (click)="isUpdate(currentAnimal)" *ngFor="let currentAnimal of animals">Species: {{currentAnimal.species}};  Name: {{currentAnimal.species}};  Age: {{currentAnimal.age}};   Caretakers:  {{currentAnimal.caretakers}} <button (click)="editAnimal()">Edit!</button></li>
+     </ul>
+     <hr>
+   <div *ngIf="selectedAnimal">
+           <div>
+            <h3>Editing Animal: </h3>
+             <h3>{{selectedAnimal.species}}</h3>
+             <h3>Edit Animal</h3>
+             <label>Enter Animal Species:</label>
+             <input [(ngModel)]="selectedAnimal.species">
+             <br>
+             <label>Enter Animal Name:</label>
+             <input [(ngModel)]="selectedAnimal.name">
+             <br>
+             <label>Enter Animal Age:</label>
+             <input type="number" name="age" [(ngModel)]="selectedAnimal.age">
+             <br>
+             <label>Enter Animal Location:</label>
+             <input [(ngModel)]="selectedAnimal.location">
+             <br>
+             <label>Enter Animal Caretakers:</label>
+             <input type="number" name="caretakers" [(ngModel)]="selectedAnimal.caretakers">
+             <br>
+           </div>
+             <button (click)="finishedEditing()">Done</button>
+       </div>
   </div>
   `
 })
 
 export class AppComponent {
-  currentAnimal: string = 'Arctic Fox';
+  currentList: string = 'Zoo Animals:';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   animals: Animal[] = [
-    new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
-    new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
-    new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud Noises")
-  ];
-   selectedAnimal = null;
+  new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool shade", "Loud noises"),
+  new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
+  new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud Noises")
+];
+selectedAnimal: Animal = this.animals[1];
 
-   editAnimal(clickedAnimal) {
-      this.selectedAnimal = clickedAnimal;
-    }
+editAnimal() {
+    alert("You just requested to edit an Animal!");
+  }
 
-    isDone(clickedAnimal: Animal) {
-        if(clickedAnimal.done === true) {
-          alert("This animal is done!");
-        } else {
-          alert("This animal is not updated.");
-        }
+isUpdate(clickedAnimal: Animal) {
+      if(clickedAnimal.update === true) {
+        alert("This animal is updated");
+      } else {
+        alert("This animal is not updated");
       }
-
-    finishedEditing() {
-      this.selectedAnimal = null;
-  }
-
-   editTask(clickedAnimal) {
-      this.selectedAnimal = clickedAnimal;
     }
-
-    priorityColor(currentAnimal){
-    if (currentAnimal.diet === 'Carnivore'){
-      return "bg-warning";
-    } else if (currentAnimal.diet === 'Herbivore'){
-      return "bg-info";
-    }
-  }
 }
 
-export class Animal {
-  public done: boolean = false;
-  constructor(public species: string, public  name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string){}
+export class Animal{
+
+public update: boolean = false;
+constructor(public species: string, public  name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string){}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
 
+
 @NgModule({
-  imports: [ BrowserModule, FormsModule  ],
+  imports: [BrowserModule, FormsModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
